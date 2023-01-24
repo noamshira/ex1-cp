7c489259cd30cfb9069d1ec7b199474cbcf503fa
Noam Shira <noamisrael10@gmail.com>
2023-01-24 20:00:08 +0200

    password security

--- example.py
+++ example.py
@@ -1,25 +1,30 @@
-print("enter the numbers")
-k=int(input())
-print('enter your message')
-message=input()
-def cropping(message,k):
-    l=len(message)
-    if l==k:
-        return (message)
-    else:
-        str=message[0:k]
-    if message[k] or message[k+1]==' ':
-        return str
-    flag=False
-    while flag == False:
-        k=k-1
-        if message[k]==' ':
-            str=message[0:k]
-            flag=True
+import sys
+ls=['!', '@', '#' , '$', '%', '^', '&','*', '(',')','_']
+print(ls)
+print("enter a  passwpord")
+ps=input()
+length=len(ps)
+if length<6:
+    sys.exit("password not long enough")
+f1=False
+f2=False
+f3=f2=False
+for char in ps:
+    if char.islower():
+        f1=True
+    elif char.isupper():
+        f2=True
+    elif char in ls:
+        f3=True
+    if char.isspace():
+        sys.exit("password not illigal")
+if f1 and f2 and f3:
+    print("True")
+else:print("False")
 
-    return str
-str=cropping(message,k)
-print(str)
+
+
+
 
 
 
